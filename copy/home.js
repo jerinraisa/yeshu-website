a8fd280097f0e83c76d0ef5df458ffb0cd598d2f
Jerin Raisa <jerin.raisa11@gmail.com>
2021-05-11 03:08:42 -0400

    first commit

---
+++ copy/home.js
@@ -0,0 +1,23 @@
+const copy = {
+  hero: {
+    title: "Hi, I’m <em>Yeshu Jain</em> 👋🏼",
+    blurb:
+      "I’m a software developer with an interest in Artificial Intelligence and Embedded Systems.",
+  },
+  aboutMe: {
+    title: "Here’s a bit about me",
+    blurb:
+      "Currently, I’m studying <em>Computer Engineering</em> at the <em>University of Waterloo</em>. This past fall, I’ve been interning as a Software Engineer at Intel, working on their FPGA based Deep Learning Accelerator. I’ll be graduating in Winter 2022.<br/><br/>For the last few years of my undergrad, I’ve been on the <em>Dean’s Honours List</em>. I also have an affinity for research, as I’ve worked as a Research Assistant under Professor Wojciech Golab in Winter 2019. Here I used numerical analysis to estimate the total energy consumption used by a household. In my freetime, I enjoy reading Fantasy Fiction such as the <em>Lord of the Rings</em> series, and playing board games with friends. Reach out if you want to hear more about my interests, research, or board game suggestions.",
+  },
+  experiences: {
+    title: "Some of my recent experiences",
+  },
+  projects: {
+    title: "Some of my recent projects",
+  },
+  footer: {
+    text: "Thanks for coming by, if you’re interested in hearing more about my work, interests, or simply nerd out about Lord of the Rings, check out my <a href='https://www.google.com/' target='_blank'>Linkedin</a>, <a href='https://www.google.com/' target='_blank'>Github</a>, or feel free to <a href='https://www.google.com/' target='_blank'>email</a> me! 👋🏼",
+  },
+};
+
+export default copy;
